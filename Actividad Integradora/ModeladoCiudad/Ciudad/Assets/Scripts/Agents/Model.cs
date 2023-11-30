47769132cbb036e617d9c0c88000f770e3bdd0ac
dunishaaa <aphimontes@outlook.com>
2023-11-29 18:19:09 -0600

    skybox y camara corregida

--- Actividad Integradora/ModeladoCiudad/Ciudad/Assets/Scripts/Agents/Model.cs
+++ Actividad Integradora/ModeladoCiudad/Ciudad/Assets/Scripts/Agents/Model.cs
@@ -6,32 +6,32 @@ using UnityEngine.Networking;
 
 public class Model: MonoBehaviour
 {
-    public float factor = 10f;
+    public int numberCars = 1, numberPedestrians = 1;
+    public float speed, rotationSpeed, pedestrianSpeedFactor = 0.22f;
+    public float errTolerance = 1f;
+
     public List<GameObject> carsList;
     public List<GameObject> metrobusList;
     public List<GameObject> pedestriansList;
-    public float speed, rotationSpeed;
-    public float xd, yd, zd;
-    public int currentStep = 0;
-
-    public int numberCars = 1;
-    public float errTolerance = 1f;
-    public float floorY = -14.1f;
 
 
-    public List<GameObject> agents;
-    public Dictionary<int, GameObject> cars;
-    public Dictionary<int, GameObject> metrobuses;
-    public Dictionary<int, GameObject> pedestrians;
 
-
-    private void Start()
+    private Dictionary<int, GameObject> cars;
+    private Dictionary<int, GameObject> metrobuses;
+    private Dictionary<int, GameObject> pedestrians;
+    private float factor = 10f;
+    private float pedestrianY = 8f, floorY = 2f;
+    private float xd, yd, zd;
+    private void Awake()
     {
         cars = new Dictionary<int, GameObject>();
         metrobuses = new Dictionary<int, GameObject>();
         pedestrians = new Dictionary<int, GameObject>();
-        InitializeModel();
+    }
 
+    private void Start()
+    {
+        InitializeModel();
     }
 
 
@@ -40,7 +40,6 @@ public class Model: MonoBehaviour
         float totalDistance = 0f;
         foreach (var kvp in cars)
         {
-            int key = kvp.Key;
             GameObject val = kvp.Value;
             Transform transform = val.GetComponent<Transform>();
             Car car = val.GetComponent<Car>();
@@ -48,14 +47,29 @@ public class Model: MonoBehaviour
 
         }
 
-        totalDistance /= (float)cars.Count;
+        foreach(var kvp in metrobuses){
+            GameObject val = kvp.Value;
+            Transform transform = val.GetComponent<Transform>();
+            Metrobus metrobus = val.GetComponent<Metrobus>();
+            totalDistance += Vector3.Distance(metrobus.targetPosition, transform.position);
+        }
+
+        foreach(var kvp in pedestrians){
+            GameObject val = kvp.Value;
+            Transform transform = val.GetComponent<Transform>();
+            Pedestrian pedestrian = val.GetComponent<Pedestrian>();
+            totalDistance += Vector3.Distance(pedestrian.targetPosition, transform.position);
+        }
+
+
+
+        totalDistance /= cars.Count + metrobuses.Count + pedestrians.Count;
 
         if(totalDistance < errTolerance)
         {
             StartCoroutine(GetData((modelData) =>
             {
                 UpdateAgents(modelData);
-                currentStep++;
             }));
         }
 
@@ -72,7 +86,7 @@ public class Model: MonoBehaviour
 
     private IEnumerator GetDataInit(Action<ModelData> callback)
     {
-        string url = "http://127.0.0.1:5000/init/" + numberCars;
+        string url = "http://127.0.0.1:5000/init/" + numberCars + "/" + numberPedestrians;
         using (UnityWebRequest getRequest = UnityWebRequest.Get(url))
         {
             yield return getRequest.SendWebRequest();
@@ -95,7 +109,7 @@ public class Model: MonoBehaviour
     private IEnumerator GetData(Action<ModelData> callback)
     {
 
-        string url = "http://127.0.0.1:5000/data/" + currentStep;
+        string url = "http://127.0.0.1:5000/data/" + 0;
 
         using (UnityWebRequest getRequest = UnityWebRequest.Get(url))
         {
@@ -148,7 +162,6 @@ public class Model: MonoBehaviour
     }
     private void CreateAgents(ModelData data)
     {
-        currentStep++;
         //Cars
         foreach(AgentData agentData in data.cars)
         {
@@ -197,14 +210,15 @@ public class Model: MonoBehaviour
                 cars[agent.id] = newCar;
                 break;
             case 1:
-                //randomIndex = UnityEngine.Random.Range(0, metrobusList.Count);
-                //GameObject newMetrobus= metrobusList[randomIndex];
-                //newMetrobus = Instantiate(newMetrobus, spawnPosition, looking_to);
-                //metrobuses[agent.id] = newMetrobus;
+                randomIndex = UnityEngine.Random.Range(0, metrobusList.Count);
+                GameObject newMetrobus= metrobusList[randomIndex];
+                newMetrobus = Instantiate(newMetrobus, spawnPosition, looking_to);
+                metrobuses[agent.id] = newMetrobus;
                 break;
             case 2:
                 randomIndex = UnityEngine.Random.Range(0, pedestriansList.Count);
                 GameObject newPedestrian = pedestriansList[randomIndex];
+                spawnPosition.y = pedestrianY;
                 newPedestrian = Instantiate(newPedestrian, spawnPosition, looking_to);
                 pedestrians[agent.id] = newPedestrian;
                 break;
@@ -232,19 +246,20 @@ public class Model: MonoBehaviour
                 car.targetPosition = nextDirection;
                 break;
             case 1:
-               // currentAgent = metrobuses[agent.id];
-                //Metrobus metrobus = currentAgent.GetComponent<Metrobus>();
-                //metrobus.speed = speed;
-                //metrobus.rotationSpeed = rotationSpeed;
-                //metrobus.targetPosition = nextDirection;
+                currentAgent = metrobuses[agent.id];
+                Metrobus metrobus = currentAgent.GetComponent<Metrobus>();
+                metrobus.speed = speed;
+                metrobus.rotationSpeed = rotationSpeed;
+                metrobus.targetPosition = nextDirection;
                 break;
             case 2:
                 currentAgent = pedestrians[agent.id];
                 Pedestrian pedestrian = currentAgent.GetComponent<Pedestrian>();
-                pedestrian.speed = speed;
-                pedestrian.rotationSpeed = rotationSpeed;
+                pedestrian.speed = speed*pedestrianSpeedFactor;
+                nextDirection.y = pedestrianY;
                 pedestrian.targetPosition = nextDirection;
                 break;
+
             default:
                 break;
         }
